8dde4fb6a8d1902e8e694866557194799e7e1cd9
Akinjide Bankole <akinjide.bankole@andela.com>
2016-10-31 01:11:28 +0100

    style(Grunt): adjust spacing and reconf grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,62 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        babel: {
-			options: {
-				sourceMap: true,
-				presets: ['es2015']
-			},
-            build: {
-                expand: true,
-              	src: ['**/*.es6.js'],
-              	dest: './build',
-              	ext: '.js'
-            }
-		},
-		eslint: {
-			options: {
-				format: 'node_modules/eslint-json',
-				outputFile: `${__dirname}/logs/eslint/${'<%= pkg.name %>'}-checkstyle.json`,
-				configFile: 'config/eslint.json',
-				rulePaths: ['config/rules']
-			},
-			build: ['**/*.js', 'bin/www', '!node_modules/**']
-		},
-		nodemon: {
-			dev: {
-				script: '<%= pkg.scripts.dev %>',
-				options: {
-					callback: function(nodemon) {
-						nodemon.on('log', function(event) {
-							console.log(event.colour + '\n\t' + event.message);
-						});
-					},
-					env: {
-						PORT: '1334'
-					},
-					cwd: __dirname,
-					ignore: ['node_modules/**'],
-					ext: 'es6.js',
-                    watch: [__dirname]
-				}
-			}
-		},
-		clean: {
-			build: ['build/']
-		},
-        concurrent: {
-            dev: {
-                tasks: ['nodemon', 'watch'],
-                options: {
-                    logConcurrentOutput: true
-                }
-            }
-        },
-		watch: {
-			scripts: {
-				files: ['**/*.es6.js'],
-				tasks: ['clean', 'build']
-			}
-		}
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    babel: {
+      options: {
+        sourceMap: true,
+        presets: ['es2015']
+      },
+      build: {
+        expand: true,
+        src: ['app/**/*.js', './config/*.js', './index.js', './bin/www'],
+        dest: './build'
+      }
+    },
+    eslint: {
+      options: {
+        format: 'node_modules/eslint-json',
+        outputFile: `${__dirname}/logs/eslint/${'<%= pkg.name %>'}-checkstyle.json`,
+        // configFile: 'config/eslint.json',
+        // rulePaths: ['config/rules']
+      },
+      build: ['**/*.js', 'bin/www', '!node_modules/**']
+    },
+    nodemon: {
+      dev: {
+        script: '<%= pkg.scripts.dev %>',
+        options: {
+          callback: function(nodemon) {
+            nodemon.on('log', function(event) {
+              console.log(event.colour + '\n\t' + event.message);
+            });
+          },
+          env: {
+            PORT: '1334'
+          },
+          cwd: __dirname,
+          ignore: ['node_modules/**'],
+          ext: '.js',
+          watch: [__dirname]
+        }
+      }
+    },
+    clean: {
+      build: ['build/']
+    },
+    concurrent: {
+      dev: {
+        tasks: ['nodemon', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['<%= babel.build.src %>'],
+        tasks: ['clean', 'build']
+      }
+    }
   });
 
   // Grunt plugins.
@@ -66,6 +65,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-concurrent');
 
   // Grunt Tasks.
